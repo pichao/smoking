7aafd58c42c34987e93128d73e769f4a262be1dc
evan <evan@gmail.com>
2025-04-29 22:56:22 +0800

    feat: 图片价格

--- src/assets/data/smokings.ts
+++ src/assets/data/smokings.ts
@@ -29,21 +29,21 @@ const data = [
 
     {
         name: '和天下(中支（硬白尊享）)',
-        price:'900马币/条',
-        des:'8mg',//焦油含量
+        price:'950马币/条',
+        des:'10mg',//焦油含量
 
         smokingUrl:require('../smokes/和天下硬白尊享.png')
     },
     {
         name: '和天下(粗支（硬包）)',
-        price:'900马币/条',
-        des:'8mg',//焦油含量
+        price:'950马币/条',
+        des:'11mg',//焦油含量
 
         smokingUrl:require('../smokes/和天下硬包粗支.jpg')
     },
     {
         name: '和天下(细支（硬包）)',
-        price:'800马币/条',
+        price:'900马币/条',
         des:'8mg',//焦油含量
 
         smokingUrl:require('../smokes/和天下细支硬包.jpg')
@@ -68,29 +68,29 @@ const data = [
         price:'900马币/条',
         des:'8mg',//焦油含量
 
-        smokingUrl:require('../smokes/云烟黑金刚.png')
+        smokingUrl:require('../smokes/南京九五.jpg')
     },
     {
         name: '南京九五至尊（细支（硬包））',
         price:'850马币/条',
         des:'8mg',//焦油含量
 
-        smokingUrl:require('../smokes/云烟黑金刚.png')
+        smokingUrl:require('../smokes/南京九五细.png')
     },
     {
         name: '大重九（粗支（软包））',
-        price:'900马币/条',
+        price:'950马币/条',
         des:'8mg',//焦油含量
 
-        smokingUrl:require('../smokes/云烟黑金刚.png')
+        smokingUrl:require('../smokes/软大重九.png')
     },
 
     {
         name: '大重九（细支（硬包））',
-        price:'800马币/条',
+        price:'900马币/条',
         des:'8mg',//焦油含量
 
-        smokingUrl:require('../smokes/云烟黑金刚.png')
+        smokingUrl:require('../smokes/细大重九.png')
     },
 
     {
@@ -116,14 +116,14 @@ const data = [
     },
     {
         name: '中华细金边（细支（硬包））',
-        price:'700马币/条',
+        price:'750马币/条',
         des:'8mg',//焦油含量
 
         smokingUrl:require('../smokes/中华细金边.png')
     },
     {
         name: '硬中华（粗支（硬包））',
-        price:'380马币/条',
+        price:'450马币/条',
         des:'8mg',//焦油含量
         smokingUrl:require('../smokes/硬中华.png')
     },
@@ -136,15 +136,15 @@ const data = [
     },
    
     {
-        name: '黄鹤楼侠骨柔情（粗支（硬包））',
-        price:'330马币/条',
+        name: '黄鹤楼侠骨柔情粗支',
+        price:'350马币/条',
         des:'8mg',//焦油含量
 
         smokingUrl:require('../smokes/黄鹤楼(硬侠骨柔情).png')
     },
     {
-        name: '黄鹤楼侠骨柔情（细支（硬包））',
-        price:'300马币/条',
+        name: '黄鹤楼侠骨柔情细支',
+        price:'330马币/条',
         des:'8mg',//焦油含量
 
         smokingUrl:require('../smokes/黄鹤楼峡谷柔情硬细支.png')
